68fc1d49102a017b8e9d08b0ed1b521b8b40d8e6
JT <hellares98@gmail.com>
2024-12-17 19:36:52 -0500

    segundo cambio

--- src/main.ts
+++ src/main.ts
@@ -24,7 +24,7 @@ async function bootstrap() {
   
   await app.listen(envs.port);
 
-  console.log('primer cambio');
+  console.log('segundo cambio');
 
   logger.log(`${CONSOLE_COLORS.STYLE.UNDERSCORE}${CONSOLE_COLORS.TEXT.CYAN}Gateway running on ${envs.port}`);
 }
