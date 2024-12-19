61b80c6b8772817edff489d05142b9cfd6e5f502
JT <hellares98@gmail.com>
2024-12-18 19:06:18 -0500

    tercer cambio

--- src/main.ts
+++ src/main.ts
@@ -24,7 +24,7 @@ async function bootstrap() {
   
   await app.listen(envs.port);
 
-  console.log('segundo cambio');
+  console.log('tercer cambio');
 
   logger.log(`${CONSOLE_COLORS.STYLE.UNDERSCORE}${CONSOLE_COLORS.TEXT.CYAN}Gateway running on ${envs.port}`);
 }
